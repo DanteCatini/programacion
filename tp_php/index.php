aadd54f63f4ffeb33bc8338c75cd7dc24e42b0d7
DanteCatini <163876885+DanteCatini@users.noreply.github.com>
2025-11-14 18:43:16 -0300

    Update index.php

--- tp_php/index.php
+++ tp_php/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Calculadora de Gastos</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@5.3.0/dist/minty/bootstrap.min.css">
     <link rel="stylesheet" href="assets/estilos.css">
 </head>
 <body>
@@ -78,4 +78,5 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
 
 </body>
+
 
